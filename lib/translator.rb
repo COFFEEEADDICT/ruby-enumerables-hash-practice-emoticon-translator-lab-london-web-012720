f8b92b4642c2628c72bf5e1e7815a495564f2305
Bryn Heath <brynheath1@gmail.com>
2020-01-25 14:28:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', e_emot)
     end
 end
 ######################################################################
-def get_english_meaning
+def get_english_meaning(file_path = './lib/emoticons.yml', e_emot)
   # code goes here
 end
 ######################################################################
