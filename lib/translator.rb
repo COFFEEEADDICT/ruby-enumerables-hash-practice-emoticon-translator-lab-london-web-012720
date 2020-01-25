3ba6c47a66f554226027cf34b3c80a85092e7b98
Bryn Heath <brynheath1@gmail.com>
2020-01-25 14:17:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,7 @@ emotes = YAML.load_file(file_path)
 new_emot = {"get_meaning" => {}, "get_emoticon" => {}}
       emotes.each do |meaning,value|
         new_emot["get_meaning"][value[1]] = meaning
-        new_emot["get_emoticon"][value[0]] = value[1]
-        #pp new_emot
+        new_emot["get_emoticon"][value[0]] = value[1] #
       end
 return new_emot
 end
