9e5e83b77210c37a661cbdd6e0764d0bf905234a
Bryn Heath <brynheath1@gmail.com>
2020-01-25 14:07:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,10 +2,11 @@ require "yaml"
 ######################################################################
 def load_library(file_path)
 emotes = YAML.load_file(file_path)
-pp emotes
-      emotes.each do |key,value|
-        emotes[:get_meaning] = key
-        #pp emotes
+new_emot = {"get_meaning" => {}, "get_emoticon" => {}}
+      emotes.each do |meaning,value|
+        new_emot["get_meaning"] = meaning
+        new_emot["get_emoticon"] = value[0]
+        #pp new_emot
       end
 return emotes
 end
