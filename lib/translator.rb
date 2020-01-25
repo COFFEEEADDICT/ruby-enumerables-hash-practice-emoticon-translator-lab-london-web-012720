c8c7256251f52fb190e07b8317fc07f4c68eb00a
Bryn Heath <brynheath1@gmail.com>
2020-01-25 14:24:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', e_emot)
     if res["get_emoticon"].include?(e_emot)
         res["get_emoticon"][e_emot]
     else
-      "Sorry, that emoticon was not found"
+      "Sorry"
     end
 end
 ######################################################################
